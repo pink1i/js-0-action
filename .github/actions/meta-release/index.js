9e203906f4fe2f1477a269070e1951742775f4c8
hongtm-1355 <tran.manh.hong@sun-asterisk.com>
2021-08-31 09:02:51 +0700

    init

---
+++ .github/actions/meta-release/index.js
@@ -0,0 +1,35 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+(async () => {
+  try {
+      const token = core.getInput('token');
+      const octokit = github.getOctokit(token);
+
+      const owner = github.context.repo.owner;
+      const repo = github.context.repo.repo;
+
+      const eventName = github.context.eventName;
+
+
+      console.log(token, octokit, owner, repo, eventName)
+      if (eventName !== 'release') {
+          core.setFailed(`Should be run by a release event but event name = ${eventName}`);
+      }
+      const releaseId = github.context.payload.release.id;
+
+      const release = await octokit.rest.repos.getRelease({
+          owner: owner,
+          repo: repo,
+          release_id: releaseId
+      });
+
+      let desc = release.data.body;
+      if (desc == null || typeof(desc) === 'undefined') {
+          desc = "";
+      }
+      console.log(desc)
+  } catch (error) {
+      core.setFailed(error.message);
+  }
+})();
